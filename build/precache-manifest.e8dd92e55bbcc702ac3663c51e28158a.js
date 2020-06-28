1fd48d9ab43ed95f0180790fe17762f53fc8a669
Maxel <gigaabait@yandex.ru>
2020-06-29 03:48:05 +0400

    done

---
+++ build/precache-manifest.e8dd92e55bbcc702ac3663c51e28158a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7a3b9db915aab81465b54e925d5ae27",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "106a58035afe8df290fa",
+    "url": "/weather/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "95f825bad364e38db1cb",
+    "url": "/weather/static/css/main.96789d2a.chunk.css"
+  },
+  {
+    "revision": "106a58035afe8df290fa",
+    "url": "/weather/static/js/2.26c714f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.26c714f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95f825bad364e38db1cb",
+    "url": "/weather/static/js/main.4c9313aa.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  },
+  {
+    "revision": "135dd48484ddff3539204d5afb6733c5",
+    "url": "/weather/static/media/picachu-error.135dd484.png"
+  }
+]);
--- src/services/api-service.js
+++ src/services/api-service.js
@@ -1,5 +1,5 @@
 export default class ApiService {
-    _apiBase = 'http://api.openweathermap.org/data/2.5/weather'
+    _apiBase = 'https://api.openweathermap.org/data/2.5/weather'
     apikey = '70e1ed322b02acbc57d443dd91065f3e'
     async getWeather(lat, lon) {
         const res = await fetch(`${this._apiBase}?lat=${lat}&lon=${lon}&appid=${this.apikey}&lang=ru`)
